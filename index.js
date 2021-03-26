c86d6681a46749743b198cbd7f02e1da712c78da
Jessica Stayton <jessica.stayton@gmail.com>
2021-03-26 22:43:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,29 +11,29 @@ function divide() {
 }
 
 function add(a,b) {
-  return a + b
+  return a + b;
 }
 
 function subtract(a,b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a,b) {
-  return a * b
+  return a * b;
 }
 
 function divide(a,b) {
-  return a / b
+  return a / b;
 }
 
 function increment(n) {
-  return n += 1
+  return n += 1;
 }
 
 function decrement(n) {
-  return n -= 1
+  return n -= 1;
 }
 
-function makeInt(n, 10) {
-  return parseInt("n" , 10)
+function makeInt(n) {
+  return parseInt(n , 10);
 }
